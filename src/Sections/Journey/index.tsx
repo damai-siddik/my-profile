59c7288b88c783b969dbed1aa73399f409ca85e6
Damai Yulianti Siddik <damai.siddik@gmail.com>
2024-03-19 15:07:52 +0700

    update journey

--- src/Sections/Journey/index.tsx
+++ src/Sections/Journey/index.tsx
@@ -72,7 +72,10 @@ export default function Journey() {
     return (
         <section id="qualification" className="pt-8 pb-16 qualification lg:max-w-[968px] m-auto">
             <h2 className="text-center font-semibold text-2xl md:text-4xl text-title mb-8">Personal Journey</h2>
-
+            <div className="flex flex-col gap-3 mx-6 text-justify">
+                <div>I am a graduate in economics who has a strong foundation in economic theory, analysis, and research. As I engaged in discussions about the developments in the world during the COVID-19 pandemic, I realized that information technology was rapidly advancing and exerting a significant influence across various sectors.</div>
+                <div>Delving deeper into this field, I discovered a newfound passion for technology and its transformative potential in diverse industries. Recognizing the increasing demand for IT skills, I decided to switch my career in late 2020 and took several bootcamps to support my skills for it. By mid-2021, I joined a company in Singapore, <span className="font-semibold cursor-pointer hover:text-primary" onClick={() => window.open("https://theforest.ai/",)}>Forest Ai Pte. Ltd</span>, as a professional software engineer and was involved in several projects.</div>
+            </div>
             <div className="flex flex-row items-center">
                 <LineStepper data={education} />
                 <Player src={graduate} autoplay loop className="w-fit hidden md:block" />
